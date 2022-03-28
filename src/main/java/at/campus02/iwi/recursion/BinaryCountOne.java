9918c2ec78ed8956311176bd7d286397bd9c4ce8
QianYangBubendorff <qian.yangbubendorff@gmail.com>
2022-03-28 17:44:15 +0200

    recursion binaryone

--- src/main/java/at/campus02/iwi/recursion/BinaryCountOne.java
+++ src/main/java/at/campus02/iwi/recursion/BinaryCountOne.java
@@ -2,32 +2,30 @@ package at.campus02.iwi.recursion;
 
 public class BinaryCountOne {
     public static void main(String[] args) {
-        int[] arr = {0, 0, 0, 0, 0, 0, 1, 1};
+        int[] arr = {0, 0, 0, 0, 0, 0, 1, 1,1};
         System.out.println(countOne(arr, 0, arr.length - 1));
     }
 
     public static int countOne(int[] array, int left, int right) {
-        int count=0;
-        while (left <= right) {
-            int middle = (left + right) / 2;
-            if (array[middle] == 0) {
-                if (array[middle + 1] == 1) {
-                    count = middle;
-                }else countOne(array, left + 1, right);
-            }
+        int count = 0;
+        if (array == null || array.length == 0) {
+            return 0;
+        }
 
-                }return count;
-            }
-//                else if (array[middle] == 1) {
-//                    if (array[middle - 1] == 0) {
-//                        count= (middle + 1);
-//                    } else {
-////                        left = middle - 1;
-//                        countOne(array, left, right-1);
-//                    }
-//                }
-            }
+        if (array[right] == 0) {
+            return 0;
+        }
 
+        // if the first array element is 1, all its elements
+        if (array[left] == 1) {
+            return (right - left + 1);
+        }
+
+        // divide the array into left and right sub array and recur
+        int mid = (left + right) / 2;
+        return countOne(array, left, mid) + countOne(array, mid + 1, right);
+    }
+}
 
 
 
