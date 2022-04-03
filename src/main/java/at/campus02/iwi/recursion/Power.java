e6e8393024b4f2e90c75ee171aed07ae31e6748e
QianYangBubendorff <qian.yangbubendorff@gmail.com>
2022-04-03 14:32:35 +0200

    revised one recursion code

--- src/main/java/at/campus02/iwi/recursion/Power.java
+++ src/main/java/at/campus02/iwi/recursion/Power.java
@@ -16,6 +16,8 @@ public class Power {
 
     public static int powerSimple1(int x, int n){
         int potenz=0;
+        if(n==0){
+        return 1;}
         if(n>0&&n%2==0){
             potenz=powerSimple(x,n/2)*powerSimple(x,n/2);
         }else if(n>0&&n%2!=0){
